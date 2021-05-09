b62f45e84ba93696eb994dcdff3db6db31f1239f
chemscn <40618598+DTK13@users.noreply.github.com>
2021-05-09 00:26:11 -0400

    Styling changes, pagination, subscriptions, loading spinner

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,18 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RandomUsers, User } from '../models';
-import { map } from 'rxjs/operators';
+import { finalize, map, switchMap, tap } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private _apiBase = 'https://randomuser.me/api'
+  private _apiBase = 'https://randomuser.me/api';
+
   constructor(private http: HttpClient) { }
 
-  getRandomUsers = (): Observable<User[]> => {
-    return this.http.get<RandomUsers>(`${this._apiBase}/?results=66`)
+  queryUsers = (pageNumber: number): Observable<User[]> => {
+    const query = `?page=${pageNumber}&results=33&seed=main`
+    return this.http.get<RandomUsers>(`${this._apiBase}/${query}`)
       .pipe(
         map((response) => response.results as User[])
       );
@@ -21,17 +23,16 @@ export class UserService {
 
   getUsersByGender = (isFemale: boolean = false): Observable<User[]> => {
     const gender = isFemale ? 'female' : 'male'
-    return this.http.get<RandomUsers>(`${this._apiBase}/?results=66&gender=${gender}`)
+    return this.http.get<RandomUsers>(`${this._apiBase}/?results=99&gender=${gender}`)
       .pipe(
-        map((response) => response.results as User[])
+        map((response) => response.results as User[]),
       );
   }
 
   getSeededUserSet = (): Observable<User[]> => {
-    return this.http.get<RandomUsers>(`${this._apiBase}/?results=66&seed=main`)
+    return this.http.get<RandomUsers>(`${this._apiBase}/?page=3&results=99&seed=main`)
       .pipe(
         map((response => response.results as User[]))
       );
   }
-
 }
